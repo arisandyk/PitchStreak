2e16d011c2bd360a92325c4b1e17578c713e4dca
Ari Sandy Kurniawan <arisandyk32@gmail.com>
2026-08-19 08:43:13 +0700

    Initial Commit

---
+++ PitchStreak/PitchStreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  PitchStreakApp.swift
+//  PitchStreak
+//
+//  Created by Ari Sandy Kurniawan on 19/08/26.
+//
+
+import SwiftUI
+
+@main
+struct PitchStreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
